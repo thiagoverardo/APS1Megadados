599814eaba9e14da0adc3a0baee516183e99f15d
vitorcm2 <vitorcm2@al.insper.edu.br>
2020-09-10 11:19:04 -0300

    Finalizando rotas

--- main.py
+++ main.py
@@ -18,6 +18,14 @@ class EditDescription(BaseModel):
     id: int
     description: str
 
+class Status(BaseModel):
+    id: int
+    status: bool
+
+
+class Delete(BaseModel):
+    id: int
+
 
 tasks = {1: {"name": "tarefinha", "description": "oi", "status": False},
          2: {"name": "tarefa", "description": "tchau", "status": True},
@@ -40,6 +48,24 @@ async def create_item(task: Task):
 async def edit_description(description: EditDescription):
     if description.id > 0 and description.id <= len(tasks):
         tasks[description.id].update({"description": description.description})
+        return tasks
+    else:
+        return tasks
+
+
+@app.put("/tasks/edit_status/")
+async def edit_status(status: Status):
+    if status.id > 0 and status.id <= len(tasks):
+        tasks[status.id].update({"status": status.status})
+        return tasks
+    else:
+        return tasks
+
+
+@app.put("/tasks/delete_task/")
+async def delete_task(delete: Delete):
+    if delete.id > 0 and delete.id <= len(tasks):
+        tasks.pop(delete.id)
         return tasks
     else:
         return tasks
